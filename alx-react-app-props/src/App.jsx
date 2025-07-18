eb5b7bb9c5a192384dd988665da95b6606395aca
Teeto <moatasem.elemam20@gmail.com>
2025-07-18 17:32:59 +0300

    Updated4

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,8 +7,8 @@ import Header from "./components/Header.jsx";
 import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
 import UserProfile from "./components/UserProfile.jsx";
-import ProfilePage from "./ProfilePage.jsx";
-import UserContext from "./UserContext.js";
+import ProfilePage from "./components/ProfilePage.jsx";
+import UserContext from "./components/UserContext.js";
 function App() {
   const [count, setCount] = useState(0);
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };

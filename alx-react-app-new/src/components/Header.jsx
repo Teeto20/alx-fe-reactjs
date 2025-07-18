1a35d49f884d4a471bb0fefbb78371444e70d3de
Teeto <moatasem.elemam20@gmail.com>
2025-07-18 03:35:11 +0300

    Updated

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
-    <header>
-      <h1>My Favorite Cities</h1>
+    <header
+      style={{
+        backgroundColor: "#282c34",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ color: "white" }}>My Favorite Cities</h1>
     </header>
   );
 };

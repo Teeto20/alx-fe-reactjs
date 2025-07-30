939927dfab3d52fb6f3e02dc461c3857744c3eed
Teeto <moatasem.elemam20@gmail.com>
2025-07-30 22:45:31 +0300

    updated3

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -18,7 +18,7 @@ function Search({ onSearch }) {
       const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }

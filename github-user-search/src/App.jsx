c6b83aba3ab3ca9b971bcbb63de852137cc21525
Elmo3tasem <moatasem.elemam20@gmail.com>
2025-07-31 13:07:22 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Search from "./components/Search";
 import "./index.css"; // Ensure Tailwind CSS is imported
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />,
         <Route path="/" element={<Search />} />
+        <Route path="/home" element={<Home />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>

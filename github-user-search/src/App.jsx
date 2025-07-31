d32634c9385b7e0916a08f774c8475778e47b9e7
Teeto <moatasem.elemam20@gmail.com>
2025-07-31 09:11:10 +0300

    updated5

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Search from "./components/Search";
-
+import "./index.css"; // Ensure Tailwind CSS is imported
 function App() {
   return (
     <Router>

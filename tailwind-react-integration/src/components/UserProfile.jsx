8becb15d7e321e8ed49436507691ae80504adaef
Teeto <moatasem.elemam20@gmail.com>
2025-08-07 19:02:43 +0300

    styling in advanced way

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,15 @@
 function UserProfile() {
   return (
-    <div className=" sm:max-w-xs sm:p-4  user-profile bg-gray-100 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className=" sm:max-w-xs sm:p-4  user-profile bg-gray-100 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
-        src="https://via.placeholder.com/150"
+        src="https://as2.ftcdn.net/jpg/00/24/86/11/1000_F_24861198_7J0bNWHiTYCHFa2nVZNIWFzMiTIbKxpI.jpg"
         alt="User"
-        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto"
+        className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto
+        hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">Almoatasem</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">
+        Almoatasem
+      </h1>
       <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.

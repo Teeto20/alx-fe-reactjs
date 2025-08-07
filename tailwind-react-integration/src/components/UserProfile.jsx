d247fdafa827be2efa836989d0ae9f73f127fcbe
Teeto <moatasem.elemam20@gmail.com>
2025-08-07 18:53:19 +0300

    styling for small screen

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className=" sm:max-w-xs sm:p-4  user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 sm:w-24 sm:h-24 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">Almoatasem</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl sm:text-lg text-blue-800 my-4">Almoatasem</h1>
+      <p className="text-gray-600 text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>

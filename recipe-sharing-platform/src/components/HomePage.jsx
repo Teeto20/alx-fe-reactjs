9972f6eab34dea59e1b305f7f70f19cf8c23550f
Teeto <moatasem.elemam20@gmail.com>
2025-08-11 23:06:58 +0300

    Adding homepage with styles

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import data from "../data.json";
+
+function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+  // use setRecipes(data) instead of setRecipes(data.recipes || [])
+  useEffect(() => {
+    setRecipes(data);
+  }, []);
+
+  return (
+    <div className="p-6 max-w-7xl mx auto">
+      <h1 className="text-3xl font-bold text-center mb-6 ">Recipe Lists</h1>
+      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {data.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="bg-white rounded-xl shadow-md
+            overflow-hidden hover:shadow-lg transition-shadow duration-300 p-6"
+          >
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">{recipe.title}</h2>
+              <p className="text-gray-600 mt-2">{recipe.summary}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
